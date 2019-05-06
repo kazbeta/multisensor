846645e1eebdf32053c7ecef030a2d08ef891e6a
kazbeta <45856707+kazbeta@users.noreply.github.com>
2019-05-06 09:08:46 -0700

    Update multisensor.py

--- multisensor.py
+++ multisensor.py
@@ -35,7 +35,7 @@ GPIO.setup(4, GPIO.IN)
 thread_video = threading.Thread(target = video.capture(25,60))
 #thread_audio = threading.Thread(target = )
 #thread_grideye = threading.Thread(target = grideye.GridEye.get_thermistor_temp())
-thread_temperature = threading.Thread(target = temperature.reagBME280All())
+thread_temperature = threading.Thread(target = temperature.readBME280All())
 thread_airquality = threading.Thread(target = airquality.getvalue())
 
 
