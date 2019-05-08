0ff49778725f07d573dfbeb56e54f81d1efb93cf
kazbeta <45856707+kazbeta@users.noreply.github.com>
2019-05-08 07:34:16 -0700

    Create motion_HC-SR501.py

---
+++ archive/motion_HC-SR501.py
@@ -0,0 +1,23 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+
+GPIO.setup(4, GPIO.IN) #PIR
+#GPIO.setup(24, GPIO.OUT) #BUzzer
+
+try:
+    time.sleep(2) # to stabilize sensor
+    while True:
+        if GPIO.input(4):
+#            GPIO.output(24, True)
+#            time.sleep(0.5) #Buzzer turns on for 0.5 sec
+#            GPIO.output(24, False)
+            print(time.time()),
+            print("Motion Detected...")
+            time.sleep(2) #to avoid multiple detection
+        time.sleep(0.1) #loop delay, should be less than detection delay
+
+except:
+    GPIO.cleanup()
+
